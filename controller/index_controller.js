c4d6018e0353a4835ad85d0f053bb1f16330a127
lsk <sinrim10@gmail.com>
2017-04-23 16:35:50 +0900

    1번 요구사항. 추가.

--- controller/index_controller.js
+++ controller/index_controller.js
@@ -3,7 +3,7 @@
  */
 'use strict'
 const utils = require('../utils/utils');
-
+const request = require('request');
 function crawler(req,res,next){
     utils.scrap(function(lists,err){
         if(err){
@@ -13,7 +13,45 @@ function crawler(req,res,next){
     })
 }
 
+function findBlockInfo(req,res,next) {
+    const url = "https://blockchain.info/block-index/"+req.params.hash;
+    request({
+        url: url, //URL to hit
+        method: 'GET',
+        qs:{"format":'json'}
+    }, function(error, response, body){
+        if(error){
+            return cb(null,error);
+        }
+        const result = {};
+        const json = JSON.parse(body);
+        let tx_length;
+        let value_sum = 0;
+        let size_sum = 0;
+        tx_length = json.tx.length;
+        if(Array.isArray(json.tx)){
+            let i = 0;
+            json.tx.forEach(function(k){
+                let sub_sum = 0;
+                size_sum += k.size;
+                if(Array.isArray(k.out)){
+                    k.out.forEach(function(r){
+                        sub_sum+=r.value;
+                    });
+                    value_sum += sub_sum;
+                }
+                ++i;
+            })
+        }
+        result.tx_length = tx_length;
+        result.avg_value = value_sum/tx_length;
+        result.avg_size = size_sum/tx_length;
+        result.avg_fee = json.fee/tx_length;
+        res.render('lists', { result: result });
+    });
+}
 
 module.exports = {
-    crawler : crawler
+    crawler : crawler,
+    findBlockInfo: findBlockInfo
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ const index_ctrl = require('../controller/index_controller');
 
 /* GET home page. */
 router.get('/', index_ctrl.crawler);
-
+router.get('/blocks/:hash',index_ctrl.findBlockInfo);
 module.exports = router;
